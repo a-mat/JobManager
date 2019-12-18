ab4441ee26258d8a7b67b49d15b4c09e23b765cb
June <aomathew94@gmail.com>
2019-12-17 20:28:08 -0600

    added logging back in

--- bin/directory_deletion.py
+++ bin/directory_deletion.py
@@ -18,7 +18,6 @@ import click
 
 def test (user,app,savedsearch,ttl):
     time=ttl
-
     dict={"user":user, "app":app, "savedsearch":savedsearch}
     dict = {k: v for k, v in dict.items() if v != None}
     for k,v in dict.items():
@@ -26,46 +25,34 @@ def test (user,app,savedsearch,ttl):
 
     # print(path.abspath(path.join(os.getcwd(),"../..")))
     path="dispatch"
-
     scanDir(dict,path)
-
-
 
 #path='/syslog/apps/splunk/var/run/splunk/dispatch'
 #path='dispatch'
 
 def scanDir(dict,path):
     global list_of_stuff
-
-   # logging.info("Running down the list in" + path)
+    logging.info("Running down the list in" + path)
     try:
-        #
         d=os.listdir(path)
-        #print("folder inside is going to be")
-        #print(d)
     except:
-            #print("directory doesnt exist")
+            logging.info("directory doesnt exist")
             exit()
     else:
         for dir in d:
-            #print("lets open up " + dir)
             jobpath = (path+'/'+dir)
-            #print("aka " + jobpath)
-            #logging.info("First Job to be examined is: "+ dir)
+            logging.info("First Job to be examined is: "+ dir)
             try:
-
                 jobstat=open(jobpath + "/status.csv")
 
             except FileNotFoundError:
-                ##logging.info(jobpath + "/status.csv file doesnt exist")
+                logging.info(jobpath + "/status.csv file doesnt exist")
 
-                print("/status.csv file doesnt exist")
             except OSError:
-                #logging.info("Error opening up " + jobpath + "/status.csv")
-                print("Error opening up " + jobpath + "/status.csv")
+                logging.info("Error opening up " + jobpath + "/status.csv")
+
             else:
                 reader = csv.DictReader(jobstat)
-
                 delcheck = False
                 for r in reader:
                     print(r)
@@ -74,24 +61,25 @@ def scanDir(dict,path):
                         for v in dict[k]:
 
                             if r[k]==v and r['state'] == 'DONE':
-
                                 total+=1
+                                logging.info(k + ':' + v +' matched with  ' + jobpath + '\'s ' + k + ':' + r[k] )
                             else:
-                                print("wtf")
+                                logging.info(k + ':' + v + ' did not match with  ' + jobpath + '\'s ' + k + ':' + r[k])
 
                     if total== len(dict.keys()):
                         delcheck = True
 
 
                 if delcheck:
-                    #logging.info(dir + "matches the delCheck. It is marked for deletion")
+                    logging.info(dir + " job matches the delCheck. It is marked for deletion")
                     list_of_stuff.append(jobpath)
-                #logging.info("The list of Marked jobs are: " + ','.join(list_of_stuff))
+                logging.info("The list of Marked jobs are: " + ','.join(list_of_stuff))
 
 
 
         for d in list_of_stuff:
-            print(d + "will be deleted")
+            logging.info('scan across dispatch directory is complete. Files ' + d + ' will be deleted')
+
             # shutil.rmtree(folder,ignore_errors=True)
 
 
@@ -101,25 +89,11 @@ def scanDir(dict,path):
 
 # System argument will take Username, Saved Search Name, APP,
 if __name__ == "__main__":
-
+    logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO, filename='test.log')
+    logging.info('Script ran')
     list_of_stuff = []
     test()
 
-   # try:
-    #    test()
 
-    #except:
-    #    print("there was an error with the arguments")
-    #    exit()
-    #try:
-     #   dispatchPath=sys.argv[4]
-    #except:
-    #    path="dispatch"
-    #else:
-     #   path=dispatchPath
-    #finally:
-     #   list_of_stuff=[]
-      #  scanDir()
-       # delList(list_of_stuff)
 
 
